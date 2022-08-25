608818ae68e36b1c5092d4af4170a4f9a723ca55
jespinoza12 <jespinoza@student.neumont.edu>
2022-08-25 10:39:47 -0600

    Easy medium hard button

--- anime-trivia/src/App.js
+++ anime-trivia/src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState, useEffect} from 'react'
 function App() {
+  //All Questions for Quiz
+  const [questions, setQuestions] = useState([]);
+  const [difficulty, setDifficulty] = useState("Easy")
+  useEffect(() => {
+    if (difficulty === "Easy"){
+      getEasyData()
+    }else if (difficulty === "Medium"){
+      getMediumData()
+    }else if (difficulty === "Hard"){
+      getHardData()
+    }
+  }, [difficulty])
+
+  const getHardData = () => {
+    var url = `https://opentdb.com/api.php?amount=15&category=31&difficulty=hard`;
+    fetch(url)
+      .then(r => r.json(0))
+      .then(data => {
+        setQuestions(data.results)
+        console.log(data.results)
+      }).catch(e => console.log(e));
+  }
+  const getMediumData = () => {
+    var url = `https://opentdb.com/api.php?amount=15&category=31&difficulty=medium`;
+    fetch(url)
+      .then(r => r.json(0))
+      .then(data => {
+        setQuestions(data.results)
+        console.log(data.results)
+      }).catch(e => console.log(e));
+  }
+  const getEasyData = () => {
+    var url = `https://opentdb.com/api.php?amount=15&category=31&difficulty=easy`;
+    fetch(url)
+      .then(r => r.json(0))
+      .then(data => {
+        setQuestions(data.results)
+        console.log(data.results)
+      }).catch(e => console.log(e));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Difficulty: {difficulty}</h1>
+        <button onClick={() => setDifficulty("Easy")}>Easy</button>
+        <button onClick={() => setDifficulty("Medium")}>Medium</button>
+        <button onClick={() => setDifficulty("Hard")}>Hard</button>
+        <button>Start Trivia</button>
       </header>
     </div>
   );
