d84fcb3297f1138fb34fa8e42e73a1fc99a5f1bd
jespinoza12 <jespinoza@student.neumont.edu>
2022-08-30 10:54:55 -0600

    Burh

--- anime-trivia/src/App.js
+++ anime-trivia/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {useState, useEffect} from 'react'
+import Trivia from './trivia';
 function App() {
   //All Questions for Quiz
   const [questions, setQuestions] = useState([]);
   const [difficulty, setDifficulty] = useState("Easy")
+  const [playing, setPlaying] = useState(false)
   useEffect(() => {
     if (difficulty === "Easy"){
       getEasyData()
@@ -42,14 +44,25 @@ function App() {
       }).catch(e => console.log(e));
   }
 
+  const setStart = () => {
+    setPlaying(true)
+  }
+
+
+  
   return (
+    
     <div className="App">
       <header className="App-header">
+        {
+        playing? <Trivia questions = {questions}/> : <div>
         <h1>Difficulty: {difficulty}</h1>
         <button onClick={() => setDifficulty("Easy")}>Easy</button>
         <button onClick={() => setDifficulty("Medium")}>Medium</button>
         <button onClick={() => setDifficulty("Hard")}>Hard</button>
-        <button>Start Trivia</button>
+        <button onClick={() => setStart()}>Start Trivia</button>
+        </div>
+        }
       </header>
     </div>
   );
