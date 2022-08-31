de198bcff9c88a0eb37c288cf6ee3ee7c40ad135
jespinoza12 <jespinoza@student.neumont.edu>
2022-08-30 20:47:43 -0600

    Fix

--- anime-trivia/src/App.js
+++ anime-trivia/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {useState, useEffect} from 'react'
+import Trivia from './trivia';
 function App() {
   //All Questions for Quiz
   const [questions, setQuestions] = useState([]);
   const [difficulty, setDifficulty] = useState("Easy")
+  const [start, setStart] = useState(false)
   useEffect(() => {
     if (difficulty === "Easy"){
       getEasyData()
@@ -42,19 +44,25 @@ function App() {
       }).catch(e => console.log(e));
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the Anime trivia game</h1>
-        <h1>select your difiiculty and hit start</h1>
-        <h2>Difficulty: {difficulty}</h2>
-        <button onClick={() => setDifficulty("Easy")}>Easy</button>
-        <button onClick={() => setDifficulty("Medium")}>Medium</button>
-        <button onClick={() => setDifficulty("Hard")}>Hard</button>
-        <button>Start</button>
-      </header>
-    </div>
-  );
+  if (start === true){
+    return(
+      <Trivia questions={questions}/>
+    )
+  }else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to the Anime trivia game</h1>
+          <h1>select your difiiculty and hit start</h1>
+          <h2>Difficulty: {difficulty}</h2>
+          <button onClick={() => setDifficulty("Easy")}>Easy</button>
+          <button onClick={() => setDifficulty("Medium")}>Medium</button>
+          <button onClick={() => setDifficulty("Hard")}>Hard</button>
+          <button onClick={() => setStart(true)}>Start</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
